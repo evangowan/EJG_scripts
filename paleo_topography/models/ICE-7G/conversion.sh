364e93d29660690614813cf367a6b7d8fa2d0569
evangowan <evangowan@gmail.com>
2021-08-23 08:30:58 +0200

    conversion files for ICE-7G

---
+++ paleo_topography/models/ICE-7G/conversion.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+model_name="ICE-7G"
+url="https://www.atmosp.physics.utoronto.ca/~peltier/data.php"
+model_dir="model_files/"
+
+# time in years from the command line
+time=$1
+
+# This takes ICE-7G and puts it onto a standard grid and file format that is used for the other scripts
+
+# for comparison with PaleoMIST, I am using 0.25 degrees
+resolution=0.25
+
+
+mkdir temp
+mkdir grids
+
+# gets the file name
+
+filename=$( awk --field-separator='\t' -v time=${time} '{if ($1 == time) {print $2}}' times_files.txt)
+
+echo ${filename}
+# checks if the filename was found
+
+if [ -z "$filename" ]; then
+	echo "Time ${time} is not available for ${model_name}"
+	exit 0
+fi
+
+# checks that the files exist
+if [ ! -f "${model_dir}${filename}" ]; then
+	echo "You must download ${model_name} files from ${url}"
+	echo "error: missing ${filename}"
+	exit 0
+fi
+
+# The ICE-xG files do not include shelves
+
+time_index=$( echo ${time} | awk '{print $1/1000}' )
+
+echo ${time_index}
+
+
+gmt grdsample ${model_dir}${filename}?Topo -T -Rg -I${resolution} -Gtemp/topography.nc
+
+
+gmt grdsample ${model_dir}${filename}?stgit -T -Rg -I${resolution} -nn -Gtemp/ice_thickness.nc
+
+gmt grdmath temp/topography.nc temp/ice_thickness.nc SUB = temp/subglacial.nc
+
+mv temp/topography.nc grids/topography_${full_time}.nc
+mv temp/ice_thickness.nc grids/ice_thickness_${full_time}.nc
+
+mv temp/subglacial.nc grids/subglacial_${full_time}.nc
+
